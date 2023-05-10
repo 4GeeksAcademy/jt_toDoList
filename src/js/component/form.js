330184e4d3d05ba63d285f00f13ef2a549bbd7e7
julitar <126813769+julitar@users.noreply.github.com>
2023-05-10 19:23:17 +0000

    Primeros pasos

---
+++ src/js/component/form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Form = () => {
+
+    return 
+
+}
+
+export default Form
+
+
+// le mando como prop list y setList 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,18 @@
 import React from "react";
+import Form from "./form";
+import List from "./list";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>	
+			<div className="row">
+				<div className="col-12 d-flex justify-content-center">
+					<h1>Lista de tareas</h1>
+				
+					
+				</div>
+			</div>
+		</>
 	);
 };
 
